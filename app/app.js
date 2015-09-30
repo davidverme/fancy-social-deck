bf4baab30a117b21b4fa45f6061c3c29f121d0ec
David Verme <david.verme@gmail.com>
2015-09-30 16:10:49 -0300

    Improves for specific display size

--- app/app.js
+++ app/app.js
@@ -5,9 +5,9 @@
 var fancySocialDeckApp = angular.module('fancySocialDeck', ['ui.router', 'openfb'])
     .run(function ($rootScope, $state, $window, OpenFB) {
 
-        //OpenFB.init('1581349792132388', 'http://localhost:63342/fancy-social-desk/oauthcallback.html');
-        OpenFB.init('1581349792132388', 'http://natyydavid.com/fancysocialdeck/oauthcallback.html');
-        //OpenFB.init('866144803475098', 'http://localhost:63342/fancy-social-desk/oauthcallback.html');
+        OpenFB.init('1581349792132388', 'http://localhost/fancy-social-deck/oauthcallback.html');
+        //OpenFB.init('1581349792132388', 'http://natyydavid.com/fancysocialdeck/oauthcallback.html');
+        //OpenFB.init('866144803475098', 'http://localhost/fancy-social-deck/oauthcallback.html');
         //OpenFB.init('866144803475098', 'http://natyydavid.com/fancysocialdeck/oauthcallback.html');
 
         $rootScope.$on('$stateChangeStart', function(event, toState) {
