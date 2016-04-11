9fd1f2515b0b58214b837728eb0c97ff9778ca7f
David Verme <david.verme@gmail.com>
2016-04-11 10:42:50 -0300

    Update app.js

--- app/app.js
+++ app/app.js
@@ -5,10 +5,11 @@
 var fancySocialDeckApp = angular.module('fancySocialDeck', ['ui.router', 'openfb'])
     .run(function ($rootScope, $state, $window, OpenFB) {
 
-        OpenFB.init('1581349792132388', 'http://localhost/fancy-social-deck/oauthcallback.html');
+        //OpenFB.init('1581349792132388', 'http://localhost/fancy-social-deck/oauthcallback.html');
         //OpenFB.init('1581349792132388', 'http://natyydavid.com/fancysocialdeck/oauthcallback.html');
         //OpenFB.init('866144803475098', 'http://localhost/fancy-social-deck/oauthcallback.html');
         //OpenFB.init('866144803475098', 'http://natyydavid.com/fancysocialdeck/oauthcallback.html');
+        OpenFB.init('1581349792132388', 'http://ec2-52-10-10-135.us-west-2.compute.amazonaws.com/fancysocialdeck/oauthcallback.html');
 
         $rootScope.$on('$stateChangeStart', function(event, toState) {
             if (toState.name !== "login" && !$window.sessionStorage['fbtoken']) {
